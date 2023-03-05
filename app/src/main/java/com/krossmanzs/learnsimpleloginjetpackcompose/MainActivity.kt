d7da4d4ca5a307a92876193f347d9754ee30d9c3
krossmanzs <radi.adi78@gmail.com>
2023-03-05 14:21:44 +0700

    Complete the login ui

--- app/src/main/java/com/krossmanzs/learnsimpleloginjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/krossmanzs/learnsimpleloginjetpackcompose/MainActivity.kt
@@ -1,15 +1,45 @@
 package com.krossmanzs.learnsimpleloginjetpackcompose
 
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.widget.FrameLayout
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusOrder
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.insets.navigationBarsWithImePadding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout.RESIZE_MODE_ZOOM
+import com.google.android.exoplayer2.ui.StyledPlayerView
 import com.krossmanzs.learnsimpleloginjetpackcompose.ui.theme.LearnSimpleLoginJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +52,119 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Login(getVideoUri())
+                }
+            }
+        }
+    }
+
+    private fun getVideoUri(): Uri {
+        val rawId: Int = resources.getIdentifier("clouds", "raw", packageName)
+        val videoUri = "android.resource://$packageName/$rawId"
+        return Uri.parse(videoUri)
+    }
+}
+
+sealed class InputType(
+    val label: String,
+    val icon: ImageVector,
+    val keyboardOptions: KeyboardOptions,
+    val visualTransformation: VisualTransformation
+) {
+    object Name: InputType(
+        label = "Username",
+        icon = Icons.Default.Person,
+        KeyboardOptions(imeAction = ImeAction.Next),
+        visualTransformation = VisualTransformation.None
+    )
+    object Password: InputType(
+        label = "Password",
+        icon = Icons.Default.Lock,
+        KeyboardOptions(
+            imeAction = ImeAction.Done,
+            keyboardType = KeyboardType.Password
+        ),
+        visualTransformation = PasswordVisualTransformation()
+    )
+}
+
+private fun Context.doLogin() {
+    Toast.makeText(
+        this,
+        "Something went wrong, try again later!",
+        Toast.LENGTH_SHORT
+    ).show()
+}
+
+private fun Context.buildExoPlayer(uri: Uri): ExoPlayer =
+    ExoPlayer.Builder(this).build().apply {
+        setMediaItem(MediaItem.fromUri(uri))
+        repeatMode = Player.REPEAT_MODE_ALL
+        playWhenReady = true
+        prepare()
+    }
+
+private fun Context.buildPlayerView(exoPlayer: ExoPlayer): StyledPlayerView =
+    StyledPlayerView(this).apply {
+        player = exoPlayer
+        layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+        useController = false
+        resizeMode = RESIZE_MODE_ZOOM
+    }
+
+@Composable
+fun Login(videoUri: Uri) {
+    val context = LocalContext.current
+    // focus manager used to clear focus and close software keyboard at the end
+    val passwordFocusRequester = FocusRequester()
+    val focusManager : FocusManager = LocalFocusManager.current
+    val exoPlayer = remember {context.buildExoPlayer(videoUri)}
+
+    DisposableEffect(
+        AndroidView(factory = {it.buildPlayerView(exoPlayer)},
+        modifier = Modifier.fillMaxSize())
+    ) {
+        onDispose {
+            exoPlayer.release()
+        }
+    }
+
+    ProvideWindowInsets {
+
+        Column(
+            Modifier
+                .navigationBarsWithImePadding()
+                .padding(24.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp, alignment = Alignment.Bottom),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(painter = painterResource(id = R.drawable.ic_android_black_24dp),
+                contentDescription = "logo",
+                Modifier.size(80.dp),
+                tint = Color.Green
+            )
+            TextInput(InputType.Name, keyboardActions = KeyboardActions {
+                passwordFocusRequester.requestFocus()
+            })
+            TextInput(InputType.Password, keyboardActions = KeyboardActions(onDone = {
+                focusManager.clearFocus()
+                context.doLogin()
+            }), focusRequester = passwordFocusRequester)
+            Button(onClick = {
+                context.doLogin()
+            }, modifier = Modifier.fillMaxWidth()) {
+                Text(text = "SIGN IN", Modifier.padding(vertical = 8.dp))
+            }
+            Divider(
+                color = Color.White.copy(alpha = 0.3f),
+                thickness = 1.dp,
+                modifier = Modifier.padding(top = 48.dp),
+            )
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(text = "Don't have an account?", color = Color.White)
+                TextButton(onClick = {}) {
+                    Text(text = "SIGN UP")
                 }
             }
         }
@@ -30,14 +172,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun TextInput(
+    inputType: InputType,
+    focusRequester: FocusRequester? = null,
+    keyboardActions: KeyboardActions
+) {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LearnSimpleLoginJetpackComposeTheme {
-        Greeting("Android")
-    }
+    var value by remember { mutableStateOf("") }
+
+    TextField(
+        value = value,
+        onValueChange = {value = it},
+        modifier = Modifier
+            .fillMaxWidth()
+            .focusOrder(focusRequester ?: FocusRequester()),
+        leadingIcon = { 
+            Icon(imageVector = inputType.icon, contentDescription = null)
+        },
+        label = { Text(text = inputType.label)},
+        shape = RoundedCornerShape(12.dp),
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = Color.White,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent
+        ),
+        singleLine = true,
+        keyboardOptions = inputType.keyboardOptions,
+        visualTransformation = inputType.visualTransformation,
+        keyboardActions = keyboardActions
+    )
 }
